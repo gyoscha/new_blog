0b0d7cb8f277e7600bfa6f7dfdc15dadaf6b253a
gyoscha <sokolovgeorgy@gmail.com>
2022-08-02 21:29:45 +0300

    fix login/logout drf

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('auth/', include('rest_framework.urls')),
     path('accounts/profile/', views.AccountAPIView.as_view())
 ]
