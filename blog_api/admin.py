105b8934190299671bd490ccb15bafbc617ddbdc
gyoscha <sokolovgeorgy@gmail.com>
2022-08-02 21:14:40 +0300

    add Profile model and Note model

--- blog_api/admin.py
+++ blog_api/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+
+from . import models
 
 
+class ProfileInline(admin.StackedInline):
+    """ Добавляем модель профиля в админку, чтобы они были на одной странице """
+    model = models.Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    """ Меняем представление User в админке """
+    model = User
+    fields = ['username']   # Todo Дополнить другими полями, хотя бы имя и фамилия
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+
+
+@admin.register(models.Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
--- blog_api/models.py
+++ blog_api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(instance, created, **kwargs):
+    """ Функция создания профиля при создании пользователя и подписка на себя же """
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+
+class Note(models.Model):
+    """ Модель для записей """
+    user = models.ForeignKey(
+        User, related_name='note', on_delete=models.DO_NOTHING
+    )
+    title = models.CharField(
+        max_length=300,
+        verbose_name='Заголовок'
+    )
+    note = models.TextField(verbose_name='Текст поста')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.create_at:%Y-%m-%d %H:%M}): "
+            f"{self.title[:20]}..."
+        )
