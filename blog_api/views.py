f3e95db4e2068c3e621be35a5ee9ffa62ee745e3
gyoscha <sokolovgeorgy@gmail.com>
2022-08-03 17:38:57 +0300

    add permissions

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
-from rest_framework import permissions
+from rest_framework import permissions as rest_permissions
 from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from . import serializers
+from . import serializers, permissions
 from blog_api.models import Profile, Note
 
 
@@ -18,7 +18,7 @@ class CreateUserView(CreateAPIView):
     """ Представление для регистрации пользователя """
     model = User
     permission_classes = [
-        permissions.AllowAny   # чтобы любой новый пользователь смог зарегистрироваться
+        rest_permissions.AllowAny   # чтобы любой новый пользователь смог зарегистрироваться
     ]
     serializer_class = serializers.UserSignUpSerializer
 
@@ -34,6 +34,7 @@ class NoteAPIView(ListCreateAPIView):
 
 
 class NoteDetailAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    """ Редактирование и удаление поста """
+    permission_classes = [IsAuthenticated, permissions.OnlyAuthorEditNote]
     queryset = Note.objects.all()
     serializer_class = serializers.NoteSerializer
